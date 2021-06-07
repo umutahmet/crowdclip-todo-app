c2d0264aaf7c28078460c614935b21fa2cee1f20
Umut Ahmet <hello@umutahmet.com>
2021-06-07 22:10:08 +1000

    app.js formating

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import { ToastContainer } from 'react-toastify'
-
 import { loadUser } from './redux/actions/auth'
 import setAuthToken from './helpers/setAuthToken'
 import Header from './pages/layout/Header'
@@ -14,29 +13,29 @@ import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token)
+    setAuthToken(localStorage.token)
 }
 
 function App() {
-  useEffect(() => {
-    store.dispatch(loadUser())
-  }, [])
+    useEffect(() => {
+        store.dispatch(loadUser())
+    }, [])
 
-  return (
-    <Provider store={store}>
-      <Router>
-        <>
-          <Header />
-          <ToastContainer newestOnTop autoClose={2000} />
+    return (
+        <Provider store={store}>
+            <Router>
+                <>
+                    <Header />
+                    <ToastContainer newestOnTop autoClose={2000} />
 
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route component={Routes} />
-          </Switch>
-        </>
-      </Router>
-    </Provider>
-  )
+                    <Switch>
+                        <Route exact path="/" component={Homepage} />
+                        <Route component={Routes} />
+                    </Switch>
+                </>
+            </Router>
+        </Provider>
+    )
 }
 
 export default App
