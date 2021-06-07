a1a16709114e86d8159a736a7a9b8cb590a5a04f
Umut Ahmet <hello@umutahmet.com>
2021-06-07 20:42:55 +1000

    Montserrat font family added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const fontFamily = {
+  ...defaultTheme.fontFamily,
+  sans: ['Montserrat', 'system-ui', 'sans-serif'],
+}
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily,
     extend: {
       colors: {
         primary: '#8D11C8',
