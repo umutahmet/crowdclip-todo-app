43487e55269a227ad6ab40970935fdb8f089f1a5
Umut Ahmet <hello@umutahmet.com>
2021-06-07 21:26:14 +1000

    added homepage styles

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -10,16 +10,15 @@ const Homepage = () => {
   }
 
   return (
-    <div>
-      <h1>Crowdclip Todo App</h1>
-      <p>Create and manage todos.</p>
-      <div>
-        <Link to="/signup">
-          Sign Up
-        </Link>
-        <Link to="/signin">
-          Sign In
-        </Link>
+    <div className="min-h-screen flex flex-col justify-between">
+      <div className="flex items-center justify-center text-center pt-48 pb-16">
+        <div>
+          <h1 className="text-7xl font-black tracking-tighter mb-6">Crowdclip <br /> Todo App</h1>
+          <h2 className="text-xl">The only todo app you'll <br /> ever need.</h2>
+        </div>
+      </div>
+      <div className="container mx-auto">
+        <img src="images/product-shot.png" alt="" className="mx-auto" />
       </div>
     </div>
   )
