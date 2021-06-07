15ddd7c1e865d2cb91b9d0ba6ea72e8f59bb9f37
Umut Ahmet <hello@umutahmet.com>
2021-06-07 23:23:15 +1000

    redirecting to correct signin route in privateRoute component, reformatted

--- client/src/pages/routing/PrivateRoute.js
+++ client/src/pages/routing/PrivateRoute.js
@@ -3,20 +3,22 @@ import { Route, Redirect } from "react-router-dom"
 import { useAuth } from "./../../redux/hooks"
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { isAuthenticated, loading } = useAuth()
+    const { isAuthenticated, loading } = useAuth()
 
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        !isAuthenticated && !loading ? (
-          <Redirect to="/login" />
-        ) : (
-          <Component {...props} />
-        )
-      }
-    />
-  )
+    // console.log({isAuthenticated, loading})
+
+    return (
+        <Route
+            {...rest}
+            render={(props) =>
+                !isAuthenticated && !loading ? (
+                    <Redirect to="/signin" />
+                ) : (
+                    <Component {...props} />
+                )
+            }
+        />
+    )
 }
 
 export default PrivateRoute
