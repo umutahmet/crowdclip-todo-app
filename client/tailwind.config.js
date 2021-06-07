d8498f1a5bc9d3ec2493deb38906b0ba76b31454
Umut Ahmet <hello@umutahmet.com>
2021-06-07 20:39:22 +1000

    project colors added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#8D11C8',
+        secondary: '#0D0D0D',
+      }
+    },
   },
   variants: {
     extend: {},
