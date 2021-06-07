bc085bc3ae79dd455042ec68f20e935f80fa2462
Umut Ahmet <hello@umutahmet.com>
2021-06-07 21:56:51 +1000

    hiding header from signup and signin pages

--- client/src/pages/layout/Header.js
+++ client/src/pages/layout/Header.js
@@ -1,24 +1,30 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 const Header = () => {
-  return (
-    <div className="fixed w-full">
-        <div className="container mx-auto">
-            <div className="flex items-center justify-between p-6">
-                <img src="logo.png" alt="Crowdclip Todo App" />
-                <div>
-                    <Link to="/signin" className="text-sm font-medium hover:underline">Sign in</Link>
-                    <Link 
-                        to="/signup" 
-                        className="inline-block py-3 px-5 ml-8 bg-primary text-white text-sm font-medium tracking-wide rounded-lg transition-all hover:bg-secondary">
-                        Sign Up — FREE
-                    </Link>
+    const location = useLocation()
+    // hide header on sign up and sign in pages
+    if (['/signup', '/signin'].includes(location.pathname)) {
+        return null
+    }
+
+    return (
+        <div className="fixed w-full">
+            <div className="container mx-auto">
+                <div className="flex items-center justify-between p-6">
+                    <img src="logo.png" alt="Crowdclip Todo App" />
+                    <div>
+                        <Link to="/signin" className="text-sm font-medium hover:underline">Sign in</Link>
+                        <Link
+                            to="/signup"
+                            className="inline-block py-3 px-5 ml-8 bg-primary text-white text-sm font-medium tracking-wide rounded-lg transition-all hover:bg-secondary">
+                            Sign Up — FREE
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
